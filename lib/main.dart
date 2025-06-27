5a614a54324a9b8cc7042fb21d076de613f0c04e
waranya chimngam <aumwaranya154@gmail.com>
2025-06-27 16:07:47 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final imgeList = <String> [
+      'assets/images/aummm3.jpg',
+      'assets/images/aummm4.jpg',
+      'assets/images/aummm5.jpg',
+      'assets/images/aummm7.jpg',
+      'assets/images/aummm12.jpg',
+      'assets/images/aummm13.jpg',
+      'assets/images/aummm14.jpg',
+      'assets/images/aummm16.jpg',
+      'assets/images/aummm2.jpg',
+      'assets/images/tt1.jpg',
+
+      
+    ];
+
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          ),
+          itemCount: imgeList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              color: const Color.fromARGB(255, 53, 255, 46),
+              child: Image.asset(imgeList[index], fit: BoxFit.cover,)
+            );
+          },
         ),
       ),
     );
